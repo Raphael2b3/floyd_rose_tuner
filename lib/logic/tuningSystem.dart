c17a44cd2ae05c3bb6a772d913cf6907d477023e
Raphael2b3 <raphael.schuetz0311@gmail.com>
2024-11-27 23:55:52 +0100

    plugin hu

--- lib/logic/tuningSystem.dart
+++ lib/logic/tuningSystem.dart
@@ -1,9 +1,22 @@
+import 'dart:typed_data';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:record/record.dart';
+
 @riverpod
-Stream<int> streamExample(Ref ref) async* {
-  // Every 1 second, yield a number from 0 to 41.
-  // This could be replaced with a Stream from Firestore or GraphQL or anything else.
-  for (var i = 0; i < 42; i++) {
-    yield i;
-    await Future<void>.delayed(const Duration(seconds: 1));
+Stream<Uint8List> streamExample(Ref ref) async* {
+  final record = AudioRecorder();
+  ref.onDispose(() async {
+    await record.cancel();
+    await record.dispose();
+  });
+  // Check and request permission if needed
+  if (await record.hasPermission()) {
+    var stream = await record
+        .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
+    await for (Uint8List data in stream) {
+      yield data;
+    }
   }
 }
