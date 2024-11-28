eff0c95b6797d6707c2e1f8ed6ee8eaeac1190a8
Raphael2b3 <raphael.schuetz0311@gmail.com>
2024-11-28 15:19:57 +0100

    permission stuff

--- lib/logic/tuningSystem.dart
+++ lib/logic/tuningSystem.dart
@@ -1,22 +1,49 @@
-import 'dart:typed_data';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pitch_detector_dart/pitch_detector.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:record/record.dart';
+import 'dart:async';
+import 'dart:typed_data';
 
+
+typedef Result = int; //Uint8List;
 @riverpod
-Stream<Uint8List> streamExample(Ref ref) async* {
-  final record = AudioRecorder();
-  ref.onDispose(() async {
-    await record.cancel();
-    await record.dispose();
-  });
-  // Check and request permission if needed
-  if (await record.hasPermission()) {
-    var stream = await record
-        .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
-    await for (Uint8List data in stream) {
-      yield data;
+Stream<Result> streamExample(Ref ref) async* {}
+
+final name = StreamNotifierProvider<AudioPitch, Result>(
+    AudioPitch.new); //.someModifier<AudioPitch, Result>(AudioPitch.new);
+
+class AudioPitch extends StreamNotifier<Result> {
+
+  static const int sampleRate = 44100;
+  static const int bufferSize = 2048;
+  static const int bitsPerSample = 8;
+  static const int bitrate = 8*44100;
+
+  @override
+  Stream<Result> build() async* {
+    //Create a new pitch detector and set the sample rate and buffer size
+    final pitchDetectorDart = PitchDetector(
+        audioSampleRate: sampleRate*1, bufferSize: bufferSize);
+
+
+    var recorder = AudioRecorder();
+    var recordConfig = const RecordConfig(encoder: AudioEncoder.pcm16bits, sampleRate: sampleRate, bitRate: bitrate );
+    ref.onDispose(() async {
+      await recorder.cancel();
+      await recorder.dispose();
+    });
+
+    // Check and request permission if needed
+    if (await recorder.hasPermission()) {
+      var stream = await recorder
+          .startStream(recordConfig);
+      await for (Uint8List sample in stream) {
+        final result = pitchDetectorDart.getPitchFromIntBuffer(sample);
+
+        yield sample.length;
+      }
     }
   }
+
 }
